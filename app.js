8322068b1d3db22efda328a9d233f516afe456dc
Bendegúz Szczuka <112816411+asimoq@users.noreply.github.com>
2024-03-17 16:44:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             /* If the object is completely visible in the window, fade it it */
-            if( (bottom_of_window + 230)> bottom_of_object ){
+            if( (bottom_of_window + 450)> bottom_of_object ){
                 
                 $(this).animate({'opacity':'1'},300);
                     
